8c52e2712ccf3075a0d57a239575c62aba2e2664
josephdiniso <josephdiniso@gmail.com>
2020-08-02 09:22:00 -0400

    Final build

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ WHITE = (255,255,255)
 
 # PyGame inits
 pygame.init()
-pygame.mixer.init(22050, -8, 16, 65536 )
 pygame.display.set_caption("Snowfall")
 size = (1000, 1000)
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 clock = pygame.time.Clock()
 
 class SnowBall:
     def __init__(self):
-        self.x_orig = random.randint(0,size[0])
+        self.x_orig = random.randint(0,screen.get_width())
         self.x = self.x_orig
         self.y = -10
         self.y_velocity = random.randint(5,10)
@@ -31,6 +30,7 @@ class SnowBall:
 
 
 def main():
+    global screen
     done = False
     balls = []
     while not done:
@@ -41,14 +41,16 @@ def main():
         for ball in balls:
             ball.forward()
             pygame.draw.circle(screen, WHITE, (ball.x, ball.y), 4)
-            if ball.y > size[1]:
+            if ball.y > screen.get_height():
                 balls.remove(ball)
                 del ball
-
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (event.type == pygame.K_DOWN and (event.key == pygame.K_ESCAPE or event.key == pygame.K_Q)):
                 pygame.quit()
                 done = True
+            if event.type == pygame.VIDEORESIZE:
+                screen = pygame.display.set_mode((event.w, event.h),
+                                              pygame.RESIZABLE)
         clock.tick(30)
         pygame.display.flip()
 
